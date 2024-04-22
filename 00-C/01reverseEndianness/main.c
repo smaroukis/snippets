fe8d8b17c5da8437df04b397fc48b53c9122f0cb
Spencer <smaroukis@gmail.com>
2024-04-22 14:06:04 -0400

    01 reverse endianness

---
+++ 00-C/01reverseEndianness/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+
+typedef uint32_t (*generalTestFunct)(uint32_t); // change return type and arguments as nec.
+
+uint32_t reverseEndianness(uint32_t x)
+{
+    uint32_t tmp = 0;
+    tmp = ((x & 0xFF) << 3*8);
+    tmp |= (x & 0xFF00) << 1*8;
+    tmp |= (x & 0xFF0000) >> 1*8;
+    tmp |= (x & 0xFF000000) >> 3*8;
+    return tmp; 
+}
+
+int main(void) {
+
+    generalTestFunct test_func = &reverseEndianness;
+    
+    // test 1:
+    uint32_t in = 0x12345678;
+    uint32_t expected = 0x78563412;
+    printf("in = 0x%x\nout = 0x%x\n", in, test_func(in));
+    assert(expected == test_func(in));
+    
+    // test 2:
+    
+    // test 3:
+    
+    // test 4:
+
+    return 0;
+}
